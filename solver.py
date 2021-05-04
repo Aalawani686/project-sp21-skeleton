e1b8f6cd09669efb4d527412765ecffac228eca5
Aniruddha Alawani <aalawani@berkeley.edu>
2021-05-03 23:23:32 -0700

    Added heuristic

--- solver.py
+++ solver.py
@@ -4,6 +4,25 @@ from utils import is_valid_solution, calculate_score
 import sys
 from os.path import basename, normpath
 import glob
+from itertools import islice
+
+def k_shortest_paths(G, source, target, k, weight=None):
+    return list(islice(nx.shortest_simple_paths(G, source, target, weight=weight), k))
+
+def isIncluded(paths, op, isEdge):
+    if (isEdge):
+        for path in paths:
+            for n in range(len(path) - 1):
+                if path[n] == op[0] and path[n+1] == op[1]:
+                    return True
+
+    else:
+        for path in paths:
+            if op in path:
+                return True
+
+    return False
+
 
 
 def solve(G):
@@ -31,36 +50,9 @@ def solve(G):
     remove_weighted_edges = []
     remove_edges = []
 
-    # for i in range(c):
-    #     nodes = list(G.nodes)
-    #
-    #     while len(nodes) > 0:
-    #
-    #         node = min(nodes, key = lambda n: sum(G[u][v]["weight"] for u, v in G.edges(n)))
-    #
-    #         if (node == s or node == e):
-    #             nodes.remove(node)
-    #             continue
-    #
-    #         edges = list(G.edges(node))
-    #         for j in range(len(edges)):
-    #             edge = list(edges[j])
-    #             edge.append(G[edge[0]][edge[1]]["weight"])
-    #             edges[j] = edge
-    #
-    #         G.remove_node(node)
-    #
-    #         if (not nx.has_path(G, s, e) or not nx.is_connected(G)):
-    #             nodes.remove(node)
-    #             G.add_node(node)
-    #             G.add_weighted_edges_from(edges)
-    #         else:
-    #             remove_nodes.append(node)
-    #             remove_weighted_edges.extend(edges)
-    #             break
     r_n, r_e = c, k
     while r_n > 0 or r_e > 0:
-
+        paths = k_shortest_paths(G, s, e, 5, weight="weight")
         p = nx.shortest_path(G, source=s, target=e)
         edges = []
         nodes = []
@@ -79,15 +71,21 @@ def solve(G):
         min_Op = -1
         min_Val = float("inf")
         add_Back_Edges = []
+        best_heuristic = 0
 
         for edge in edges:
+            heuristic = 0
+            for m in range(len(paths)):
+                if isIncluded(paths, edge, True):
+                    heuristic += (10-m)*50
+
             if (r_e == 0):
                 break
             G.remove_edge(edge[0], edge[1])
             if (not nx.has_path(G, s, e)):
                 G.add_edge(edge[0], edge[1], weight = edge[2])
                 continue
-            if (nx.dijkstra_path_length(G, source=s, target=e) < min_Val):
+            if (nx.dijkstra_path_length(G, source=s, target=e) < min_Val or heuristic > best_heuristic):
                 if (isEdge == -1):
                     blank = 0
                 elif (isEdge):
@@ -98,10 +96,17 @@ def solve(G):
                     add_Back_Edges = []
                 isEdge = True
                 min_Op = edge
+                min_Val = nx.dijkstra_path_length(G, source=s, target=e)
+                best_heuristic = heuristic
             else:
                 G.add_edge(edge[0], edge[1], weight = edge[2])
 
         for node in nodes:
+            heuristic = 0
+            for m in range(len(paths)):
+                if isIncluded(paths, node, False):
+                    heuristic += (10-m)*50
+
             if (r_n == 0):
                 break
             n_edges = list(G.edges(node))
@@ -115,7 +120,7 @@ def solve(G):
                  G.add_node(node)
                  G.add_weighted_edges_from(n_edges)
                  continue
-            if (nx.dijkstra_path_length(G, source=s, target=e) < min_Val):
+            if (nx.dijkstra_path_length(G, source=s, target=e) < min_Val or heuristic > best_heuristic):
                 if (isEdge == -1):
                     blank = 0
                 elif (isEdge):
@@ -127,6 +132,8 @@ def solve(G):
                 isEdge = False
                 min_Op = node
                 add_Back_Edges.extend(n_edges)
+                min_Val = nx.dijkstra_path_length(G, source=s, target=e)
+                best_heuristic = heuristic
             else:
                 G.add_node(node)
                 G.add_weighted_edges_from(n_edges)
@@ -142,32 +149,8 @@ def solve(G):
             remove_nodes.append(min_Op)
             remove_weighted_edges.extend(add_Back_Edges)
 
-
-        # min_edge = -1
-        # min_weight = -1
-        # while len(edges) > 0:
-        #     pos = weights.index(min(weights))
-        #     min_edge = edges[pos]
-        #     min_weight = weights[pos]
-        #     G.remove_edge(min_edge[0], min_edge[1])
-        #     if (not nx.has_path(G, s, e)):
-        #         G.add_edge(min_edge[0], min_edge[1], weight = min_weight)
-        #         edges.pop(pos)
-        #         weights.pop(pos)
-        #         min_edge = -1
-        #         min_weight = -1
-        #     else:
-        #         break
-        # if (min_edge != -1):
-        #     remove_edges.append(min_edge)
-        #     temp = min_edge.copy()
-        #     temp.append(min_weight)
-        #     remove_weighted_edges.append(temp)
-
     G.add_nodes_from(remove_nodes)
     G.add_weighted_edges_from(remove_weighted_edges)
-
-
 
     return remove_nodes, remove_edges
 
@@ -192,7 +175,7 @@ if __name__ == '__main__':
     inputs.extend(glob.glob('inputs/medium/*'))
     inputs.extend(glob.glob('inputs/large/*'))
     # inputs = []
-    # inputs.append("inputs/medium/medium-243.in")
+    # inputs.append("inputs/large/large-1.in")
     total = 0
     i = 0
     for input_path in inputs:
